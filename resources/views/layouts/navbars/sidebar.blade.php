3cd07ee1404faedb2e0326b635b1141b03848d82
Yhan17 <shikamaru.nara897@gmail.com>
2020-10-27 23:46:55 -0300

    :fire: Coloca icones e menus na sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -6,54 +6,119 @@
             <img src="{{ asset('img/icon_unitins_white.png') }}" alt="Logo Unitins Branca" />
         </div>
         <ul class="nav">
-            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+            <li @if ($pageSlug=='Pagina Inicial' ) class="active " @endif>
                 <a href="{{ route('home') }}">
-                    <i class="tim-icons icon-chart-pie-36"></i>
-                    <p>{{ _('Dashboard') }}</p>
+                    <i class="fas fa-home"></i>
+                    <p>{{ _('Página Inicial') }}</p>
                 </a>
             </li>
             <li>
-                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
-                    <i class="fas fa-pencil-alt" ></i>
-                    <span class="nav-link-text" >{{ __('Laravel Examples') }}</span>
+                <a data-toggle="collapse" href="#Cadastros" >
+                    <i class="fas fa-pencil-alt"></i>
+                    <span class="nav-link-text">{{ __('Cadastros') }}</span>
                     <b class="caret mt-1"></b>
                 </a>
 
-                <div class="collapse show" id="laravel-examples">
+                <div class="collapse show" id="Cadastros">
                     <ul class="nav pl-4">
-                        <li @if ($pageSlug == 'profile') class="active " @endif>
+                        <li @if ($pageSlug=='profile' ) class="active " @endif>
                             <a href="{{ route('profile.edit')  }}">
-                                <i class="tim-icons icon-single-02"></i>
-                                <p>{{ _('User Profile') }}</p>
+                                <i class="fas fa-door-closed"></i>
+                                <p>{{ _('Ambientes') }}</p>
                             </a>
                         </li>
-                        <li @if ($pageSlug == 'users') class="active " @endif>
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
                             <a href="{{ route('user.index')  }}">
-                                <i class="tim-icons icon-bullet-list-67"></i>
-                                <p>{{ _('User Management') }}</p>
+                                <i class="fas fa-pen-nib"></i>
+                                <p>{{ _('Disciplinas') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="bx bxs-book-alt"></i>
+                                <p>{{ _('Cursos') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="icofont-newspaper"></i>
+                                <p>{{ _('Notícias') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="fas fa-user-friends"></i>
+                                <p>{{ _('Usuários') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="fas fa-question-circle"></i>
+                                <p>{{ _('Motivos de Uso') }}</p>
                             </a>
                         </li>
                     </ul>
                 </div>
             </li>
             <li>
+                <a data-toggle="collapse" href="#Movimentação" aria-expanded="true">
+                    <i class="fas fa-bars"></i>
+                    <span class="nav-link-text">{{ __('Movimentação') }}</span>
+                    <b class="caret mt-1"></b>
+                </a>
+
+                <div class="collapse show" id="Movimentação">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug=='profile' ) class="active " @endif>
+                            <a href="{{ route('profile.edit')  }}">
+                                <i class="fas fa-calendar-check"></i>
+                                <p>{{ _('Agendamentos') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug=='users' ) class="active " @endif>
+                            <a data-toggle="collapse" href="#GerarRelatorio" aria-expanded="true">
+                                <i class='fas fa-file-pdf' ></i>
+                                <span class="nav-link-text">{{ __('Relatorios') }}</span>
+                                <b class="caret mt-1"></b>
+                            </a>
+                            <div class="collapse show" id="GerarRelatorio">
+                                <ul class="nav pl-4">
+                                    <li @if ($pageSlug=='profile' ) class="active " @endif>
+                                        <a href="{{ route('profile.edit')  }}">
+                                            <i class="fas fa-chalkboard-teacher"></i>
+                                            <p>{{ _('Professor') }}</p>
+                                        </a>
+                                    </li>
+                                    <li @if ($pageSlug=='profile' ) class="active " @endif>
+                                        <a href="{{ route('profile.edit')  }}">
+                                            <i class="fas fa-globe-americas"></i>
+                                            <p>{{ _('Geral') }}</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li>
                 <a data-toggle="collapse" href="#gerenciamento" aria-expanded="true">
-                    <i class="fas fa-cogs" ></i>
-                    <span class="nav-link-text" >{{ __('Gerenciamento') }}</span>
+                    <i class="fas fa-cogs"></i>
+                    <span class="nav-link-text">{{ __('Gerenciamento') }}</span>
                     <b class="caret mt-1"></b>
                 </a>
 
                 <div class="collapse show" id="gerenciamento">
                     <ul class="nav pl-4">
-                        <li @if ($pageSlug == 'Tipo Usuario') class="active " @endif>
+                        <li @if ($pageSlug=='Tipo Usuario' ) class="active " @endif>
                             <a href="{{ route('tipousuario.index')  }}">
-                                <i class="tim-icons icon-single-02"></i>
+                                <i class="fas fa-user"></i>
                                 <p>{{ _('Tipo Usuário') }}</p>
                             </a>
                         </li>
-                        <li @if ($pageSlug == 'Tipo Ambiente') class="active " @endif>
+                        <li @if ($pageSlug=='Tipo Ambiente' ) class="active " @endif>
                             <a href="{{ route('tipoambiente.index')  }}">
-                                <i class="fas fa-cogs"></i>
+                                <i class="fas fa-tools"></i>
                                 <p>{{ _('Tipo Ambiente') }}</p>
                             </a>
                         </li>
