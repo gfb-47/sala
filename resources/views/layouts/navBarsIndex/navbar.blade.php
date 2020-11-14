bbaeaa9c339653468ff865ca20cabfb10b61a006
Yhan17 <shikamaru.nara897@gmail.com>
2020-11-14 12:20:31 -0300

    🛠Corrige Background e add Dashboard pro user adm

--- resources/views/layouts/navBarsIndex/navbar.blade.php
+++ resources/views/layouts/navBarsIndex/navbar.blade.php
@@ -1,15 +1,23 @@
-<div class="navBarIndex">
-    <a href=""><img src="{{ asset('img') }}/logo_unitins.png" alt="" height=auto width=130></a>
-    <a class="navbar-item" href="#">Home</a>
-    <a class="navbar-item" href="{{ route('profile.edit') }}">Perfil</a>
-    <div class="dropdown">
-        <button class="dropbtn">Agendamentos
-            <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content">
-            <a class="navbar-item" href="{{ route('meusagendamentos.index') }}">Meus Agendamentos</a>
-            <a class="navbar-item" href="{{ route('novoagendamento.index') }}">Agendar ambiente</a>
+<div class="navBarIndex" >
+    <div class="navBarIndex_img">
+        <a href=""><img src="{{ asset('img') }}/logo_unitins.png" alt="" height=auto width=130></a>
+    </div>
+    <div class="navbar-item-all" style="display:flex; justify-content: flex-end;flex-flow: row wrap;" >
+        <a class="navbar-item" href="#">Home</a>
+        @if(auth()->user()->tipo_usuario == 1)
+            <a class="navbar-item" href="{{ route('home') }}">Dashboard</a>
+        @endif    
+
+        <a class="navbar-item" href="{{ route('profile.edit') }}">Perfil</a>
+        <div class="dropdown" fl>
+            <button class="dropbtn">Agendamentos
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a class="navbar-item" href="{{ route('meusagendamentos.index') }}">Meus Agendamentos</a>
+                <a class="navbar-item" href="{{ route('novoagendamento.index') }}">Agendar ambiente</a>
+            </div>
+            <button class="logoutBtn" type="button">Sair</button>
         </div>
     </div>
-    <button class="logoutBtn" type="button">Sair</button>
 </div>
