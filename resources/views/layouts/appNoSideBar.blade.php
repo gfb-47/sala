b4e7e53c3758b3d1213ea5b429fc538d8bf774b7
Yhan17 <shikamaru.nara897@gmail.com>
2020-11-15 14:26:54 -0300

    💫Corrige botão de sair

--- resources/views/layouts/appNoSideBar.blade.php
+++ resources/views/layouts/appNoSideBar.blade.php
@@ -6,7 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
         <title>{{ config('app.name', 'White Dashboard') }}</title>
 
         <!-- CSS -->
