920d94b557503e05539137a0a6df9f2b9fd404f2
Yhan17 <shikamaru.nara897@gmail.com>
2020-11-15 21:36:45 -0300

    🛠Arruma Css agendamento calendario

--- resources/views/layouts/appNoSideBar.blade.php
+++ resources/views/layouts/appNoSideBar.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="{{ asset('css') }}/index.css" rel="stylesheet" />
+        <!-- <link href="{{ asset('css') }}/index.css" rel="stylesheet" /> -->
         <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 
         <!-- Favicon -->
--- resources/views/novoagendamento/modal.blade.php
+++ resources/views/novoagendamento/modal.blade.php
@@ -1,90 +1,90 @@
 <!-- Modal -->
 <style>
 </style>
-<div class="modal fade" id="staticBackdrop" data-backdrop="static" 
-data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" 
-aria-hidden="true">
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="staticBackdropLabel">
                     Novo Agendamento
                 </h5>
-                <button type="button" class="close" data-dismiss="modal" 
-                    aria-label="Close">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             {!!Form::open()
-                    ->post()
-                    ->route('novoagendamento.store')
-                    ->multipart()!!}                    
+            ->post()
+            ->route('novoagendamento.store')
+            ->multipart()!!}
             <div class="modal-body">
-                    <div class="row">
+                <div class="row">
                     <div class="col-md-12">
-                            {!!Form::select('ambiente', 'Ambiente')
-                            ->options($ambiente->prepend('Selecione...', ''))
-                            ->value($id)
-                            ->readonly()
-                            ->required() !!}
-                        </div>
-                        <div class="col-md-6">
-                            {!!Form::date('data', 'Dia do Agendamento')
-                            
-                            ->min(auth()->user()->tipo_usuario==1?'':now()->add(2, 'day')->format('Y-m-d'))
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-3">
-                            {!!Form::time('horainicio', 'Hora de inicio')
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-3">
-                            {!!Form::time('horafim', 'Hora de finalização')
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-6">
-                            {!!Form::select('curso', 'Curso')
-                            ->options($curso->prepend('Selecione...', ''))
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-6">
-                            {!!Form::select('disciplina', 'Disciplina')
-                            ->options($disciplina->prepend('Selecione...', ''))
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-6">
-                            {!!Form::select('professorresponsavel', 'Professor responsavel')
-                            ->options($prof->prepend('Selecione...', ''))
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-6">
-                            {!!Form::select('motivoutilizacao', 'Motivo da utilização')
-                            ->options($motivo->prepend('Selecione...', ''))
-                            ->required() !!}
-                        </div>                 
-                        <div class="col-md-12">
-                            {!!Form::textarea('observacao', 'Observações')
-                            ->required() !!}
-                        </div>                 
+                        {!!Form::select('ambiente', 'Ambiente')
+                        ->options($ambiente->prepend('Selecione...', ''))
+                        ->value($id)
+                        ->readonly()
+                        ->required() !!}
                     </div>
-            </div>
-            <div class="row">
-            <div class="col-md-6">
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" id="customCheck1" required="required">
-                <label class="custom-control-label" for="customCheck1">
-                    <a class="termosredirect" href="{{asset('/storage/' . $termo_de_uso->termodeuso)}}" target="_blank">Li e aceito os termos de uso</a>
-                </label>
+                    <div class="col-md-6">
+                        {!!Form::date('data', 'Dia do Agendamento')
+
+                        ->min(auth()->user()->tipo_usuario==1?'':now()->add(2, 'day')->format('Y-m-d'))
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-3">
+                        {!!Form::time('horainicio', 'Hora de inicio')
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-3">
+                        {!!Form::time('horafim', 'Hora de finalização')
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-6">
+                        {!!Form::select('curso', 'Curso')
+                        ->options($curso->prepend('Selecione...', ''))
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-6">
+                        {!!Form::select('disciplina', 'Disciplina')
+                        ->options($disciplina->prepend('Selecione...', ''))
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-6">
+                        {!!Form::select('professorresponsavel', 'Professor responsavel')
+                        ->options($prof->prepend('Selecione...', ''))
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-6">
+                        {!!Form::select('motivoutilizacao', 'Motivo da utilização')
+                        ->options($motivo->prepend('Selecione...', ''))
+                        ->required() !!}
+                    </div>
+                    <div class="col-md-12">
+                        {!!Form::textarea('observacao', 'Observações')
+                        ->required() !!}
+                    </div>
                 </div>
             </div>
-                <div class="col-md-3">
-                    <button type="button" class="btn btn-secondary  " data-dismiss="modal">Cancelar</button>
+            <div class="modal-footer">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" id="customCheck1" required="required">
+                            <label for="customCheck1">
+                                <a class="termosredirect" href="{{asset('/storage/' . $termo_de_uso->termodeuso)}}"
+                                    target="_blank">Li e aceito os termos de uso</a>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-md-6 ">
+                        <button type="submit" class="btn btn-primary  float-right">Finalizar</button>
+                        <button type="button" class="btn btn-secondary  float-right" data-dismiss="modal">Cancelar</button>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <button type="submit" class="btn btn-primary  ">Finalizar</button>
-                </div>
+
             </div>
-                    {!!Form::close()!!}
+            {!!Form::close()!!}
         </div>
     </div>
-</div>  
+</div>
