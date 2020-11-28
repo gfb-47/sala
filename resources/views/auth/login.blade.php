b4936a5b100febd1397ed85d1b7c0c87b1dcf1c3
Weberth1233 <weberth20-19@hotmail.com>
2020-11-28 11:14:52 -0300

    Melhorando login ✔

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,12 +4,12 @@
 <div class="col-lg-4 col-md-6 ml-auto mr-auto">
     <form class="form" method="post" action="{{ route('login') }}">
         @csrf
-        <div class="card card-login card-white" style="border-radius:20px; width: auto; height:350px;
+        <div class="card card-login card-white" style="border-radius:20px; height:285px;
                 box-shadow: 4px 4px 4px 2px rgba(0, 0, 0, 0.507);">
             <div class="card-header" style="height: 50px; padding-top: 30px; padding-bottom: -30px">
                 <h3 style="color: black; text-align: center; font-weight:bold; font-family:Bree Serif">Login</h3>
             </div>
-            <div class="card-body" style="margin-top: -80px; padding:40px;">
+            <div class="card-body" style="margin-top: -90px; padding:40px;">
                 <div class="input-group{{ $errors->has('cpf') ? ' has-danger' : '' }}" style="margin-bottom:22px;">
                     <div class="input-group-prepend">
                         <div class="input-group-text" style="height:33px; background-color: #EBEBEB">
@@ -31,7 +31,7 @@
             </div>
             <div class="card-footer" style="margin-top:-110px; padding: 40px">
                 <button type="submit" href="" class="btn btn-primary btn-lg btn-block mb-3" style="border-radius: 5px;
-                    height: 45px; padding: 0px; background-color: #0375D8">Entrar</button>
+                    height: 35px; padding: 0px; background-color: #0375D8">Entrar</button>
 
                 <a style="float: right" href="{{ route('password.request') }}">Esqueci minha senha</a>
             </div>
