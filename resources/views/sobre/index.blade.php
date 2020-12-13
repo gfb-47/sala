728440e0684470e79e6b0ac2ccab7825d17b27cd
Weberth1233 <weberth20-19@hotmail.com>
2020-12-13 16:39:35 -0300

    Alteração no html e css de sobre😎.

--- resources/views/sobre/index.blade.php
+++ resources/views/sobre/index.blade.php
@@ -6,31 +6,36 @@
     <div class="col-12">
         <div class="card card-chart">
             
-            <div class="card-body">
-            <span>
-            <h3>Software criado pelo curso de Sistemas de informação.</h3><br/><br/>
-
-            Disciplina: Projeto Integrador I<br/>
-            Professor: Frederico Pires Pinto<br/><br/>
-
-            Alunos:<br/>
-                -Henrique Beckmann<br/>
-                -Caio Cézar<br/>
-                -Yhan Nunes<br/>
-                -Davi Silva<br/>
-                -Rafael Freitas<br/>
-                -Gabriel Teixeira<br/>
-                -Gabriel Brito<br/>
-                -Eduardo Rocha<br/>
-                -Gelmir Baumgratz<br/>
-                -Samuell Oliveira<br/>
-                -Weberth<br/>
-                -Richard Bruno<br/>
-            </span>
+        <div class="card-body">
+            <div style="background-color: #1B59A2; margin: -18px; margin-bottom: 20px;">
+                <h3 style="font-weight: bold; color: #fff;text-align: center;
+                text-shadow: 1px 1px #000;"><span  style="position: relative;
+    top: 20px;">Software criado pelo curso de Sistemas de Informação.</span></h3><br/><br/>
             </div>
-            <div class="card-footer py-4">
+            <div style="text-align: center;">
+                <p style="font-weight: bold;">Disciplina: Projeto Integrador I.
+                 <span style="margin:20px;"> Professor: Frederico Pires Pinto.</span></p>
+
+                <p style="font-weight: bold;">Alunos:</p>
+                <div style="color: #000;text-shadow: 0px 1px #000;">
+                    -Henrique Beckmann<br/>
+                    -Caio Cézar<br/>
+                    -Yhan Nunes<br/>
+                    -Davi Silva<br/>
+                    -Rafael Freitas<br/>
+                    -Gabriel Teixeira<br/>
+                    -Gabriel Brito<br/>
+                    -Eduardo Rocha<br/>
+                    -Gelmir Baumgratz<br/>
+                    -Samuell Oliveira<br/>
+                    -Weberth Erik<br/>
+                    -Richard Bruno<br/>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer py-4">
                 
-            </div>
+        </div>
         </div>
     </div>
 </div>
